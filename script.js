a2dda1459e11401a4da9dcfa3aef958588dde029
alexmatthes <alharrisonmatthes8@gmail.com>
2025-04-06 01:45:02 -0400

    Fixed html and fixed javascript
    weeeeewooooo

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener("click", function (event) {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    if (menu.classList.contains("open") && !menu.contains(event.target) && !icon.contains(event.target)) {
+        menu.classList.remove("open");
+        icon.classList.remove("open");
+    }
+});
